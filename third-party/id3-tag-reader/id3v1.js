ec3e5e6e6d7c9d0e2f8152dee949eff75cba9bd4
Nicolas Garnier <nivco.las@gmail.com>
2016-03-29 00:41:36 +0200

    Update id3v1.js

--- third-party/id3-tag-reader/id3v1.js
+++ third-party/id3-tag-reader/id3v1.js
@@ -3,8 +3,8 @@
  * Copyright (c) 2008 Jacob Seidelin, cupboy@gmail.com, http://blog.nihilogic.dk/
  * MIT License [http://www.opensource.org/licenses/mit-license.php]
  *
- * Extended by Antonio Afonso (antonio.afonso@opera.com), Opera Software ASA
- * Modified by Antonio Afonso (antonio.afonso@gmail.com)
+ * Extended by António Afonso (antonio.afonso@opera.com), Opera Software ASA
+ * Modified by António Afonso (antonio.afonso@gmail.com)
  * Modified by Nicolas Garnier (nivco.las@gmail.com)
  */
 
